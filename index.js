06c9c7fe7d3a111c564acd4ca436451002a1a98d
Josiel Lopez <jdlopez114@gmail.com>
2022-03-09 22:42:38 -0500

    commit tree 1193ebf08ae09739e793bba118434184c5ee40b2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const people = ["Guadelupe", "Ollie", "Aki"];
+
+
+function writeCards(people, adj){
+    const newArray = [];
+    for(let i = 0; i < people.length; i++){
+        newArray.push(`Thank you, ${people[i]}, for the wonderful ${adj} gift!`);
+    }
+    return newArray;
+}
+
+writeCards();
+
+function countDown(num){
+    while (num >= 0 ){
+        console.log(num)
+        num--;
+    }
+    
+}
